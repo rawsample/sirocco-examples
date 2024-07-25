96340324792a841136f9bfaedeb4a69301224c29
rawsample <paul.olivier@eurecom.fr>
2024-07-25 15:50:18 +0200

    Add nrf LBS service

--- sirocco_peripheral_hr/src/main.c
+++ sirocco_peripheral_hr/src/main.c
@@ -23,6 +23,7 @@
 #include <zephyr/bluetooth/services/hrs.h>
 #include <zephyr/bluetooth/controller.h>
 
+#include <bluetooth/services/lbs.h>
 
 #include <dk_buttons_and_leds.h>
 
@@ -31,6 +32,10 @@
 
 #define RUN_STATUS_LED	DK_LED1
 #define CON_STATUS_LED	DK_LED2
+#define USER_LED        DK_LED3
+#define USER_BUTTON     DK_BTN1_MSK
+
+static bool app_button_state;
 
 /* Define multiple speed for advertising */
 #define SLOOOW_ADV BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_USE_NAME, \
@@ -119,6 +124,8 @@ static struct bt_conn_auth_cb auth_cb_display = {
 	.cancel = auth_cancel,
 };
 
+/* Services */
+
 static void bas_notify(void)
 {
 	uint8_t battery_level = bt_bas_get_battery_level();
@@ -145,6 +152,45 @@ static void hrs_notify(void)
 	bt_hrs_notify(heartrate);
 }
 
+
+static void app_led_cb(bool led_state)
+{
+	dk_set_led(USER_LED, led_state);
+}
+
+static bool app_button_cb(void)
+{
+	return app_button_state;
+}
+
+static struct bt_lbs_cb lbs_callbacs = {
+	.led_cb    = app_led_cb,
+	.button_cb = app_button_cb,
+};
+
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	if (has_changed & USER_BUTTON) {
+		uint32_t user_button_state = button_state & USER_BUTTON;
+
+		bt_lbs_send_button_state(user_button_state);
+		app_button_state = user_button_state ? true : false;
+	}
+}
+
+static int init_button(void)
+{
+	int err;
+
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		printk("Cannot init buttons (err: %d)\n", err);
+	}
+
+	return err;
+}
+
+
 int main(void)
 {
 	int err;
@@ -152,14 +198,20 @@ int main(void)
     /* Use a known fixed address to ease development */
     const uint8_t public_addr[BDADDR_SIZE] = {0xaa, 0xaa, 0xef, 0xbe, 0xad, 0xde};
 
-	/* Initialize LEDs
+	/* Initialize LEDs and button
 	 */
 	err = dk_leds_init();
 	if (err) {
 		printk("LEDs init failed (err %d)\n", err);
 		return 0;
 	}
-	printk("LEDs initialized\n");
+
+	err = init_button();
+	if (err) {
+		printk("Button init failed (err %d)\n", err);
+		return 0;
+	}
+	printk("LEDs & button initialized\n");
 
     /* Set advertising address */
     bt_ctlr_set_public_addr((const uint8_t *) &public_addr);
@@ -173,8 +225,20 @@ int main(void)
 	}
 	printk("Bluetooth initialized\n");
 
-    /* Initialized Sirocco IDS */
+    /* Initialize LEDs and button callbacks
+    */
+	err = bt_lbs_init(&lbs_callbacs);
+	if (err) {
+		printk("Failed to init LBS (err:%d)\n", err);
+		return 0;
+	}
+
+    /* Initialized Sirocco IDS 
+     */
+ #if defined(CONFIG_BT_SIROCCO)
 	srcc_init();
+	printk("Sirocco Bluetooth IDS initialized\n");
+#endif
 
 	/* Start advertising */
 	bt_ready();
